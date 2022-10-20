6eb1ced391e90bedbd0f179553265ac4a0404d9a
josh-codi <odamejoshua37@gmail.com>
2022-10-20 07:26:05 +0000

    data and time

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,28 +1,42 @@
-function getData(url, callBack = (res) => { }) {
-    let res;
-    fetch(`${url}`)
-        .then((res) => res.json())
-        .then((resData) => {
-            callBack(resData)
-        }).catch((err) => console.log(err))
-}
-function postData(url, body, callBack) {
-    let response;
-    fetch(`${url}`, {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    }).then((res) => res.json())
-        .then((json) => {
-            response = json
-            callBack()
-        })
-}
+// function getData(url, callBack = (res) => { }) {
+//     let res;
+//     fetch(`${url}`)
+//         .then((res) => res.json())
+//         .then((resData) => {
+//             callBack(resData)
+//         }).catch((err) => console.log(err))
+// }
+// function postData(url, body, callBack) {
+//     let response;
+//     fetch(`${url}`, {
+//         method: "POST",
+//         body: JSON.stringify(body),
+//         headers: {
+//             'Content-type': 'application/json'
+//         }
+//     }).then((res) => res.json())
+//         .then((json) => {
+//             response = json
+//             callBack()
+//         })
+// }
 
-const base = "https://jsonplaceholder.typicode.com/";
-const base2 = "https://cryptudeapibackend.herokuapp.com/api/v1/cryptos";
+// const base = "https://jsonplaceholder.typicode.com/";
+// const base2 = "https://cryptudeapibackend.herokuapp.com/api/v1/cryptos";
 
-// getData(base2, (res)=>{console.log(res)})
-// postData(base, body, () => { console.log('what') })
+// // getData(base2, (res)=>{console.log(res)})
+// // postData(base, body, () => { console.log('what') })
+
+// Setting current date and time
+setInterval(() => {
+    const full_date = new Date().toLocaleDateString(); //Date String
+    const full_time = new Date().toLocaleTimeString(); // Time String
+    document.querySelector(".todayDate").innerText = `${full_date}, ${full_time}`
+}, 1000);
+
+
+let isLoggedIn = false;
+localStorage.setItem("isLoggedIn", true)
+isLoggedIn = Boolean(localStorage.getItem("isLoggedIn"))
+
+console.log(isLoggedIn)
